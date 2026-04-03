eafe4e56c9f8ede59996159bd299a053f0caf53a
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2026-04-03 13:10:23 +0300

    Implement data cleaning function
    Added a function to clean data by removing missing values and converting categorical variables.

--- R/02_clean_data.R
+++ R/02_clean_data.R
@@ -1,1 +1,15 @@
+library(dplyr)
+source("R/utils.R")
 
+clean_data <- function(df) {
+
+  log_message("Cleaning data...")
+
+  # Remove missing values
+  df <- df %>% na.omit()
+
+  # Convert categorical variables
+  df <- convert_to_factor(df, c(FACTOR_1, FACTOR_2))
+
+  return(df)
+}
